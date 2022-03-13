5c116aaac05f1a2dd967c86dd25d98f7a629494e
SamB440 <sam@islandearth.net>
2022-03-13 15:17:26 +0000

    Cleanup & 1.1.9 release

--- velocity/src/main/java/com/convallyria/forcepack/velocity/ForcePackVelocity.java
+++ velocity/src/main/java/com/convallyria/forcepack/velocity/ForcePackVelocity.java
@@ -29,10 +29,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
 
 @Plugin(
         id = "forcepack",
