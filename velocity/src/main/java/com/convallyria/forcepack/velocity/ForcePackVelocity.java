6bba000446c70afed92abba73aecc453ce74c114
SamB440 <sam@islandearth.net>
2024-01-04 12:53:28 +0000

    Fix velocity version

--- velocity/src/main/java/com/convallyria/forcepack/velocity/ForcePackVelocity.java
+++ velocity/src/main/java/com/convallyria/forcepack/velocity/ForcePackVelocity.java
@@ -62,7 +62,7 @@ import java.util.stream.Collectors;
 @Plugin(
         id = "forcepack",
         name = "ForcePack",
-        version = "1.3.0",
+        version = "1.3.1",
         description = "Force players to use your server resource pack.",
         url = "https://www.convallyria.com",
         dependencies = {
