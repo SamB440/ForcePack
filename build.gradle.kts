de70a8ab13617f2574808ebaae3feae389887085
SamB440 <sam@islandearth.net>
2024-04-02 12:56:16 +0100

    1.3.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
 
 allprojects {
     group = "com.convallyria.forcepack"
-    version = "1.3.3"
+    version = "1.3.4"
 
     apply(plugin = "com.github.johnrengelman.shadow")
     apply(plugin = "java")
