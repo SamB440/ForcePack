016b5a50e23b4b63c443c51294e9d44096653649
SamB440 <sam@islandearth.net>
2024-12-16 20:36:16 +0000

    1.3.7

--- velocity/src/main/java/com/convallyria/forcepack/velocity/ForcePackVelocity.java
+++ velocity/src/main/java/com/convallyria/forcepack/velocity/ForcePackVelocity.java
@@ -59,7 +59,7 @@ import java.util.stream.Collectors;
 @Plugin(
         id = "forcepack",
         name = "ForcePack",
-        version = "1.3.6",
+        version = "1.3.7",
         description = "Force players to use your server resource pack.",
         url = "https://www.convallyria.com",
         dependencies = {
