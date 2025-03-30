b2f438e636636f8d81fe5ff33324a49b6f2ae17b
SamB440 <sam@islandearth.net>
2025-03-30 15:55:17 +0100

    Add publishing of api

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    `maven-publish`
+}
+
+
 repositories {
     maven("https://repo.opencollab.dev/main/")
 }
@@ -12,4 +17,31 @@ dependencies {
 
     compileOnly("org.geysermc.geyser:api:2.4.2-SNAPSHOT")
     compileOnly("org.geysermc.floodgate:api:2.2.3-SNAPSHOT")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("forcepack") {
+            from(components["java"])
+
+            // skip shadow jar from publishing. Workaround for https://github.com/johnrengelman/shadow/issues/651
+            val javaComponent = components["java"] as AdhocComponentWithVariants
+            javaComponent.withVariantsFromConfiguration(configurations["shadowRuntimeElements"]) { skip() }
+        }
+    }
+
+    repositories {
+        // See Gradle docs for how to provide credentials to PasswordCredentials
+        // https://docs.gradle.org/current/samples/sample_publishing_credentials.html
+
+        maven {
+            val isSnapshot = true//ver.contains("SNAPSHOT")
+            name = if (isSnapshot) "snapshots" else "releases"
+            url = uri("https://repo.convallyria.com/$name/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
 }
